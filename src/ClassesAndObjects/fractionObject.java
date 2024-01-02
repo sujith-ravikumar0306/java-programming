42f8ca2283ea9c24f1953bb2f195eae94343ea60
sujith <sujithravikumar046@gmail.com>
2024-01-02 22:21:51 +0530

    first commit from office system

--- src/ClassesAndObjects/fractionObject.java
+++ src/ClassesAndObjects/fractionObject.java
@@ -4,7 +4,13 @@ import ClassesAndObjects.fractionClass;
 
 public class fractionObject {
     public static void main(String[] args) {
-        fractionClass f1 = new fractionClass(2, 3);
+        fractionClass f1 = new fractionClass(4, 8);
+        f1.print();
+        f1.iterator();
+        f1.print();
+        f1.iterator();
+        f1.print();
+        f1.iterator();
         f1.print();
         f1.iterator();
         f1.print();
